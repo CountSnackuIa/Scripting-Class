77fecbc45727b3b7ec743cf152af9217c03c3d50
CountSnackuIa <mylesmmoore@hotmail.com>
2016-04-14 11:02:39 -0600

    Update Music.cs

--- Music.cs
+++ Music.cs
@@ -10,7 +10,8 @@ public class Music : MonoBehaviour {
 	public List<GameObject> cubes;
 	public List<GameObject> speakers;
 	public List<float> times;
-	private float[] dropsArray = new float[25];
+	private float[] dropsArray = new float[]{29.856f, 30.314f, 31.253f, 32.145f, 33.037f, 33.470f, 33.928f, 34.844f, 35.735f, 36.651f, 37.109f, 37.519f, 38.410f, 39.326f, 40.217f, 
+		40.699f, 41.133f, 42.049f, 42.940f, 43.832f, 44.314f, 44.748f, 45.639f, 46.531f, 47.47f};
 	private int cubeCounter;
 
 	void Start () {
@@ -19,35 +20,11 @@ public class Music : MonoBehaviour {
 			MeshRenderer meshIsActive = item.GetComponent<MeshRenderer>();
 			meshIsActive.enabled = false;
 		}
+
 		for( int i = 0; i < 200; i++)
 			{
 				times.Add (1 + (i * .227f));
 			}
-		dropsArray[0] = 29.856f;
-		dropsArray[1] = 30.314f;
-		dropsArray[2] = 31.253f;
-		dropsArray[3] = 32.145f;
-		dropsArray[4] = 33.037f;
-		dropsArray[5] = 33.470f;
-		dropsArray[6] = 33.928f;
-		dropsArray[7] = 34.844f;
-		dropsArray[8] = 35.735f;
-		dropsArray[9] = 36.651f;
-		dropsArray[10] = 37.109f;
-		dropsArray[11] = 37.519f;
-		dropsArray[12] = 38.410f;
-		dropsArray[13] = 39.326f;
-		dropsArray[14] = 40.217f;
-		dropsArray[15] = 40.699f;
-		dropsArray[16] = 41.133f;
-		dropsArray[17] = 42.049f;
-		dropsArray[18] = 42.940f;
-		dropsArray[19] = 43.832f;
-		dropsArray[20] = 44.314f;
-		dropsArray[21] = 44.748f;
-		dropsArray[22] = 45.639f;
-		dropsArray[23] = 46.531f;
-		dropsArray[24] = 47.47f;
 
 		musicClip.Play();
 		StartCoroutine ("SyncToAudio");
@@ -56,7 +33,6 @@ public class Music : MonoBehaviour {
 
 	IEnumerator SyncBassToAudio(){
 		for (int k = 0; k < dropsArray.Length; k++){
-			// find the sample number equivalent:
 			float sampleCalc = dropsArray[k] * musicClip.clip.frequency;
 			while (musicClip.timeSamples < sampleCalc) yield return 0; // wait till the desired sample
 			foreach(GameObject item in speakers)
