0acf57685343eea0f7a749761e37b1e0039bdad9
CountSnackuIa <mylesmmoore@hotmail.com>
2016-04-14 10:40:59 -0600

    Update Music.cs

--- Music.cs
+++ Music.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System;
@@ -10,46 +10,44 @@ public class Music : MonoBehaviour {
 	public List<GameObject> cubes;
 	public List<GameObject> speakers;
 	public List<float> times;
-	public List<float> drops;
+	private float[] dropsArray = new float[25];
 	private int cubeCounter;
 
-	// Use this for initialization
 	void Start () {
 		foreach(GameObject item in speakers)
 		{
 			MeshRenderer meshIsActive = item.GetComponent<MeshRenderer>();
 			meshIsActive.enabled = false;
 		}
-
 		for( int i = 0; i < 200; i++)
 			{
 				times.Add (1 + (i * .227f));
 			}
-		drops.Add (29.856f);
-		drops.Add (30.314f);
-		drops.Add (31.253f);
-		drops.Add (32.145f);
-		drops.Add (33.037f);
-		drops.Add (33.470f);
-		drops.Add (33.928f);
-		drops.Add (34.844f);
-		drops.Add (35.735f);
-		drops.Add (36.651f);
-		drops.Add (37.109f);
-		drops.Add (37.519f);
-		drops.Add (38.410f);
-		drops.Add (39.326f);
-		drops.Add (40.217f);
-		drops.Add (40.699f);
-		drops.Add (41.133f);
-		drops.Add (42.049f);
-		drops.Add (42.940f);
-		drops.Add (43.832f);
-		drops.Add (44.314f);
-		drops.Add (44.748f);
-		drops.Add (45.639f);
-		drops.Add (46.531f);
-		drops.Add (47.47f);
+		dropsArray[0] = 29.856f;
+		dropsArray[1] = 30.314f;
+		dropsArray[2] = 31.253f;
+		dropsArray[3] = 32.145f;
+		dropsArray[4] = 33.037f;
+		dropsArray[5] = 33.470f;
+		dropsArray[6] = 33.928f;
+		dropsArray[7] = 34.844f;
+		dropsArray[8] = 35.735f;
+		dropsArray[9] = 36.651f;
+		dropsArray[10] = 37.109f;
+		dropsArray[11] = 37.519f;
+		dropsArray[12] = 38.410f;
+		dropsArray[13] = 39.326f;
+		dropsArray[14] = 40.217f;
+		dropsArray[15] = 40.699f;
+		dropsArray[16] = 41.133f;
+		dropsArray[17] = 42.049f;
+		dropsArray[18] = 42.940f;
+		dropsArray[19] = 43.832f;
+		dropsArray[20] = 44.314f;
+		dropsArray[21] = 44.748f;
+		dropsArray[22] = 45.639f;
+		dropsArray[23] = 46.531f;
+		dropsArray[24] = 47.47f;
 
 		musicClip.Play();
 		StartCoroutine ("SyncToAudio");
@@ -57,10 +55,9 @@ public class Music : MonoBehaviour {
 	}
 
 	IEnumerator SyncBassToAudio(){
-		for (int k = 0; k < drops.Count; k++){
-			// find the sample number equivalent to time_s[k]:
-			float sampleCalc = drops[k] * musicClip.clip.frequency;
-//			print (nSample);
+		for (int k = 0; k < dropsArray.Length; k++){
+			// find the sample number equivalent:
+			float sampleCalc = dropsArray[k] * musicClip.clip.frequency;
 			while (musicClip.timeSamples < sampleCalc) yield return 0; // wait till the desired sample
 			foreach(GameObject item in speakers)
 			{
@@ -73,15 +70,11 @@ public class Music : MonoBehaviour {
 				MeshRenderer meshIsActive = item.GetComponent<MeshRenderer>();
 				meshIsActive.enabled = !meshIsActive.enabled;
 			}
-//			MeshRenderer meshIsActive = speakers[speakerCounter].GetComponent<MeshRenderer>();
-//			meshIsActive.enabled = !meshIsActive.enabled;
 		}
 	}
 	IEnumerator SyncToAudio(){
 		for (int k = 0; k < times.Count; k++){
-			// find the sample number equivalent to time_s[k]:
 			float sampleCalc = times[k] * musicClip.clip.frequency;
-//			print (nSample);
 			while (musicClip.timeSamples < sampleCalc) yield return 0; // wait till the desired sample
 			if (cubeCounter >= cubes.Count)
 				cubeCounter = 0;
